7d0064ff993c10e5299a2258e9700cab34d9793c
Joe Barrow <joseph.d.barrow@gmail.com>
2016-11-18 16:51:09 -0500

    Outputting Substitution Matrices

--- scripts/substitution.py
+++ scripts/substitution.py
@@ -11,20 +11,28 @@ def write_substitution_matrix(filename, S):
         for i, note in enumerate(notes):
             f.write('{0}\t{1}\n'.format(note, '\t'.join(list(map(str, S[i, :])))))
 
+def gaussian(mu, s):
+    return lambda x: 1. / (np.sqrt(2 * np.pi) * s) * \
+        np.exp(-(x - mu) ** 2 / (2 * s ** 2))
+
+def range_around(index):
+    centered = np.zeros(88)
+    for i in range(index+1, 88):
+        centered[i] = i - index
+    for i in range(0, index):
+        centered[i] = index - i
+    return centered
+
 if __name__ == '__main__':
     distribution = np.atleast_2d(np.loadtxt('distribution.csv'))
-    M, S = np.zeros(88), np.zeros(88)
+    M, S = np.zeros((88, 88)), np.zeros((88, 88))
 
-    #gaussian = lambda x, s, a: 1. / np.sqrt(2 * np.pi * s ** 2) * \
-    #    np.exp(-(x - a) ** 2 / 2 * s ** 2)
+    for i in range(88):
+        M[:, i] = gaussian(0, 2)(range_around(i))
+        S[:, i] = np.log(M[:, i] / distribution[0, i])
 
-    #for i in range(88):
-    #    for j in range(88):
-    #        M[i, j] = gaussian(i, 1, )
-
-    #for i in range(88):
-    #    S[:, j] = np.log(M[:, j] / distribution[j])
-
-    S = np.identity(88, dtype='int32')
-    #np.savetxt('substitution.txt', S, delimiter=' ', fmt='%i')
-    write_substitution_matrix('substitution.txt', S)
+    S = S.astype('int32')
+    S[S<-10] = -10
+    
+    write_substitution_matrix('gaussian.txt', S)
+    write_substitution_matrix('identity.txt', np.identity(88, dtype='int32'))
