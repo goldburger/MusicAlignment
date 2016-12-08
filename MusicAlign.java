bd9c3be57f95b5086e488db14de415fc773fa808
Matt Goldberg <goldburger@users.noreply.github.com>
2016-12-07 19:16:32 -0500

    Removal of inverse cdf functions

--- MusicAlign.java
+++ MusicAlign.java
@@ -37,27 +37,6 @@ public class MusicAlign {
   public static ArrayList<Double> zeroVector;
   public static boolean alignGlobal = false, alignPoly = false;
 
-  public static double rationalApproximation(double t) {
-    // Abramowitz and Stegun formula 26.2.23.
-    // The absolute value of the error should be less than 4.5 e-4.
-    double c[] = {2.515517, 0.802853, 0.010328};
-    double d[] = {1.432788, 0.189269, 0.001308};
-    return t - ((c[2]*t + c[1])*t + c[0]) / (((d[2]*t + d[1])*t + d[0])*t + 1.0);
-  }
-
-  public static double normalCDFInverse(double p) {
-    if (p < 0.5)
-    {
-      // F^-1(p) = - G^-1(p)
-      return -rationalApproximation(Math.sqrt(-2.0*Math.log(p)));
-    }
-    else
-    {
-      // F^-1(p) = G^-1(1-p)
-      return rationalApproximation(Math.sqrt(-2.0*Math.log(1-p)));
-    }
-  }
-
   public static double subCost(Object i, Object j) {
     if (i instanceof String && j instanceof String) {
       return subMatrix[stringMapping.get(i)][stringMapping.get(j)];
